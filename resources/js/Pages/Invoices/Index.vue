576dd44ecdf46c53614358edccd534d7b31e9096
Craig Harman <craig@harmonicnewmedia.com>
2019-06-17 14:03:44 +0800

    Finished invoice details

--- resources/js/Pages/Invoices/Index.vue
+++ resources/js/Pages/Invoices/Index.vue
@@ -7,21 +7,25 @@
     <div class="bg-white rounded shadow overflow-x-auto">
       <table class="w-full whitespace-no-wrap">
         <tr class="text-left font-bold">
-          <th class="px-6 pt-6 pb-4 cursor-pointer" @click="orderBy('ID')">ID</th>
-		  <th class="px-6 pt-6 pb-4 cursor-pointer" @click="orderBy('status')">Status</th>
+          <th class="px-6 pt-6 pb-4 cursor-pointer" @click="orderBy('documentNumber')">Invoice Number</th>
+		      <th class="px-6 pt-6 pb-4 cursor-pointer" @click="orderBy('status')">Status</th>
+          <th class="px-6 pt-6 pb-4 cursor-pointer" @click="orderBy('amount.value')">Value</th>
+          <th class="px-6 pt-6 pb-4 cursor-pointer" @click="orderBy('subscriptionName')">Plan</th>
           <th class="px-6 pt-6 pb-4"></th>
         </tr>
         <tr v-for="invoice in invoices" :key="invoice.id" @click="show(invoice)" class="hover:bg-grey-lightest focus-within:bg-grey-lightest cursor-pointer">
           <td class="border-t px-6 py-4 flex items-center">
-            {{ invoice.id }}
+            {{ invoice.documentNumber }}
           </td>
-		  <td class="border-t">
+		      <td class="border-t">
             {{ invoice.status }}
           </td>
+          <td class="border-t">
+            ${{ invoice.amount.value }}
+          </td>
+          <td class="border-t">{{ invoice.subscriptionName }}</td>
           <td class="border-t w-px">
-            <inertia-link class="px-4 flex items-center" :href="route('customers.show', invoice.id)" tabindex="-1">
               <icon name="cheveron-right" class="block w-6 h-6 fill-grey" />
-            </inertia-link>
           </td>
         </tr>
         <tr v-if="invoices.length === 0">
@@ -31,6 +35,13 @@
     </div>
 
     <modal v-if="invoice !== null" :showing="showModal" @close="showModal = false">
+      <h1 class="text-gray-900 font-bold text-xl mb-2">
+        {{ invoice.documentNumber }}
+      </h1>
+      Created: {{ invoice.date }} Due: {{ invoice.dueDate }}
+      <div class="sub-header">{{ invoice.status }}</div>
+      <h2 class="font-bold">Total: ${{ invoice.amount.value }}</h2>
+      <h3 class="mt-2">{{ invoice.subscriptionName }}</h3>
     </modal>
     
   </layout>
--- resources/js/Shared/MainMenu.vue
+++ resources/js/Shared/MainMenu.vue
@@ -20,7 +20,7 @@
     </div>
     <div class="mb-4">
       <inertia-link class="flex items-center group py-3" :href="route('invoices')">
-        <icon name="office" class="w-4 h-4 mr-2" :class="isUrl('invoices') ? 'text-white fill-current' : 'text-primary-400 fill-current group-hover:text-white fill-current'" />
+        <icon name="dollar" class="w-4 h-4 mr-2" :class="isUrl('invoices') ? 'text-white fill-current' : 'text-primary-400 fill-current group-hover:text-white fill-current'" />
         <div :class="isUrl('invoices') ? 'text-white' : 'text-primary-400 group-hover:text-white'">Invoices</div>
       </inertia-link>
     </div>
