b9d2c0ce40110100c1efb53cb09097df247c26b7
Craig Harman <craig@harmonicnewmedia.com>
2019-06-24 11:10:22 +0800

    User table converted

--- resources/js/Shared/InertiaTable.vue
+++ resources/js/Shared/InertiaTable.vue
@@ -62,9 +62,15 @@ export default {
 		type: Object,
 		default: null
 	},
-	columns: Array,
+	columns: {
+		type: Array,
+		default: null
+	},
 	data: Array,
-	columnDefs: Array,
+	columnDefs: {
+		type: Array,
+		default: () => []
+	},
 	id: {
 		type: String,
 		default: 'id'
@@ -80,39 +86,39 @@ export default {
   watch: {
     form: {
       handler: _.throttle(function() {
-        let query = _.pickBy(this.form)
-        this.$inertia.replace(this.route(routeName, Object.keys(query).length ? query : { remember: 'forget' }))
+		let query = _.pickBy(this.form)
+        this.$inertia.replace(this.route(this.routeName, Object.keys(query).length ? query : { remember: 'forget' }))
       }, 150),
       deep: true,
 	},
 	filters: {
-		hander: function() {
-			this.form = {
-				search: this.filters.search,
-				orderColumn: this.order.orderColumn,
-				orderDirection: this.order.orderDirection,
-				trashed: this.filters.trashed,
-			}
+		handler: function() {
+			this.createFormData();
 		},
 		deep: true
 	},
-	orderColumn: {
-		hander: function() {
-			this.form = {
-				search: this.filters.search,
-				orderColumn: this.order.orderColumn,
-				orderDirection: this.order.orderDirection,
-				trashed: this.filters.trashed,
-			}
+	order: {
+		handler: function() {
+			this.createFormData();
 		},
 		deep: true
 	}
+  },
+  created: function() {
+	  if (this.columns === null) {
+		  // If no specific columns mentioned, use keys from data array
+		  this.columns = Array.from( this.data.keys() );
+	  }
+	  if (this.filters !== null || this.order !== null) {
+		  this.createFormData();
+	  }
   },
   methods: {
 	columnData: function(item, column) {
 		if (item.hasOwnProperty(column)) {
 			return item[column];
 		}	
+
 		var columnData = this.columnDefs.find(columnDef => {
 			return columnDef.hasOwnProperty(column);
 		});
@@ -126,6 +132,55 @@ export default {
 	sentanceCase: function(text) {
 		const result = text.replace(/([A-Z]+)/g, " $1").replace(/([A-Z][a-z])/g, " $1");
 		return result.charAt(0).toUpperCase() + result.slice(1);
+	},
+	formHasChanged: function() {
+		if (this.form == null) {
+			if (this.filters !== null || this.orderBy !== null) {
+				return true;
+			}
+		}
+		
+		if (this.filters.search !== this.form.search) {
+			return true;
+		}
+		if (this.filters.trashed !== this.form.trashed) {
+			return true;
+		}
+		if (this.order.orderColumn !== this.form.orderColumn) {
+			return true;
+		}
+		if (this.order.orderDirection !== this.form.orderDirection) {
+			return true;
+		}
+		return false;
+	},
+	createFormData: function() {
+		// Don't change anything if form data and filter/order data match
+		if (!this.formHasChanged()) {
+			return;
+		}
+
+		var filters = {
+			search: '',
+			trashed: ''
+		}
+		var order = {
+			orderColumn: '',
+			orderDirection: 'asc'
+		}
+		if (this.filters !== null) {
+			filters = this.filters;
+		}
+		if (this.order !== null) {
+			order = this.order;
+		}
+		
+		this.form = {
+			search: filters.search,
+			orderColumn: order.orderColumn,
+			orderDirection: order.orderDirection,
+			trashed: filters.trashed,
+		}
 	},
 	reset() {
       this.form = _.mapValues(this.form, () => null)
